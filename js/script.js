b05179dfa3d4e3ccdbcbe8ebe775aed10ddc642e
simoneaucello <simoneaucello@gmail.com>
2024-02-21 15:57:11 +0100

    [add] html structure

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// const kmTravel = parseFloat (prompt('Quanti km vuoi percorrere?'));
+// console.log(kmTravel); 
+
+// const agePassenger = parseFloat (prompt('Quanti anni hai?'));
+// console.log(agePassenger);
+
+// const kmPrice = 0.21;
+// console.log(kmPrice);
+
+
+// let totPrice = kmTravel * kmPrice;    
+// console.log(totPrice);  
+
+// if(agePassenger < 18){
+//   totPrice = totPrice - (totPrice * 20 / 100);
+// }else if(agePassenger > 65){
+//   totPrice = totPrice - (totPrice * 40 / 100);
+// }
+
+// console.log(totPrice);
+
+// document.getElementById('price').innerHTML = ` 
+// <p>Il prezzo del tuo biglietto è di:
+// ${parseFloat(totPrice).toFixed(2)}&euro;</p>
+// `;
+
+const nameSurname = document.querySelector('.name');
+const kmTravel = document.querySelector('km');
