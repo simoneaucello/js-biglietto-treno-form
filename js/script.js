57c49e80517aff8b0421c5f643eb88526190974c
simoneaucello <simoneaucello@gmail.com>
2024-02-21 17:36:57 +0100

    [add] javascript

--- js/script.js
+++ js/script.js
@@ -1,29 +1,68 @@
-// const kmTravel = parseFloat (prompt('Quanti km vuoi percorrere?'));
-// console.log(kmTravel); 
-
-// const agePassenger = parseFloat (prompt('Quanti anni hai?'));
-// console.log(agePassenger);
-
-// const kmPrice = 0.21;
-// console.log(kmPrice);
-
-
-// let totPrice = kmTravel * kmPrice;    
-// console.log(totPrice);  
-
-// if(agePassenger < 18){
-//   totPrice = totPrice - (totPrice * 20 / 100);
-// }else if(agePassenger > 65){
-//   totPrice = totPrice - (totPrice * 40 / 100);
-// }
-
-// console.log(totPrice);
 
 // document.getElementById('price').innerHTML = ` 
 // <p>Il prezzo del tuo biglietto è di:
 // ${parseFloat(totPrice).toFixed(2)}&euro;</p>
 // `;
 
+let ageUser;
+let kmUser;
+
+const kmPrice = 0.21;
+const ticket = document.querySelector('.ticket');
+
+// INPUT 
 const nameSurname = document.querySelector('.name');
-const kmTravel = document.querySelector('km');
-const agePassenger = document.querySelector('age');
+const kmTravel = document.querySelector('.km');
+const agePassenger = document.querySelector('.age');
+
+
+// BTN 
+const generate = document.querySelector('.generate');
+const reset = document.querySelector('.reset');
+
+
+console.log(ticket);
+
+
+generate.addEventListener('click', function(){
+
+  console.log(agePassenger.value);
+  console.log(kmTravel.value);
+
+  ageUser = parseInt(agePassenger.value);
+  kmUser = parseInt(kmTravel.value);
+
+  let totPrice = kmUser * kmPrice; 
+
+  if(ageUser < 18){
+  totPrice = totPrice - (totPrice * 20 / 100);
+  }else if(ageUser > 65){
+  totPrice = totPrice - (totPrice * 40 / 100);
+  }
+
+
+  console.log(ageUser);
+  console.log(kmUser);
+  console.log(totPrice);
+
+
+  document.getElementById('price').innerHTML = ` 
+  <p>Il prezzo del tuo biglietto è di:
+  ${parseFloat(totPrice).toFixed(2)}&euro;</p>
+  `;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
