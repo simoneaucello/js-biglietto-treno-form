2a39c67a888a27d2eb081b637813d4b244dc3e76
simoneaucello <simoneaucello@gmail.com>
2024-02-21 19:01:13 +0100

    [add] content to html css js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@
 
 let ageUser;
 let kmUser;
+let nameUser;
+let car;
 
 const kmPrice = 0.21;
 const ticket = document.querySelector('.ticket');
@@ -21,7 +23,9 @@ const generate = document.querySelector('.generate');
 const reset = document.querySelector('.reset');
 
 
-console.log(ticket);
+generate.addEventListener('click',function(){
+  ticket.classList.toggle('d-none');
+})
 
 
 generate.addEventListener('click', function(){
@@ -31,26 +35,61 @@ generate.addEventListener('click', function(){
 
   ageUser = parseInt(agePassenger.value);
   kmUser = parseInt(kmTravel.value);
+  nameUser = (nameSurname.value);
 
   let totPrice = kmUser * kmPrice; 
 
   if(ageUser < 18){
   totPrice = totPrice - (totPrice * 20 / 100);
+
+  document.getElementById('offer').innerHTML = ` 
+  <p>Sconto Minorenni</p>
+  `;
   }else if(ageUser > 65){
   totPrice = totPrice - (totPrice * 40 / 100);
+
+  document.getElementById('offer').innerHTML = ` 
+  <p>Sconto Over 65</p>
+  `;
+  }else{
+    document.getElementById('offer').innerHTML = ` 
+  <p>Biglietto Standard</p>
+  `;
+
   }
 
 
   console.log(ageUser);
   console.log(kmUser);
   console.log(totPrice);
+  console.log(nameUser);
 
-
+//  PREZZO BIGLIETTO 
   document.getElementById('price').innerHTML = ` 
-  <p>Il prezzo del tuo biglietto è di:
-  ${parseFloat(totPrice).toFixed(2)}&euro;</p>
+  <p>${parseFloat(totPrice).toFixed(2)}&euro;</p>
   `;
 
+  // NOME USER 
+  document.getElementById('passenger').innerHTML = nameUser;
+
+  // CARROZZA 
+  document.getElementById('carrozza').innerHTML = 
+  car = Math.floor((Math.random() * 10) + 1);
+
+  // CP CODE 
+  document.getElementById('cpCode').innerHTML = 
+  ticketNumb = Math.floor(Math.random() * (999999 - 100000 + 1) ) + 100000;
+
+})
+
+
+
+// RESET 
+reset.addEventListener('click',function(){
+  ticket.classList.toggle('d-none');
+  agePassenger.value = '';
+  kmTravel.value = '';
+  nameSurname.value = '';
 })
 
 
